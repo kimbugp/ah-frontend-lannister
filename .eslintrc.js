bb8126edbd0bf1e3da0087b3abf7f20929548bf8
Amos Welike <37612799+AmosWels@users.noreply.github.com>
2018-10-30 16:53:27 +0300

    ch(add):setup up an empty react project (#1)
    - add new react application using create-react-app
    - remove unnecessary comments from index.html
    - add tests folder to the root
    - add package-lock.json to gitignore
    - remove imports from App.js
    - delete favicon.ico
    - delete manifest.json
    - delete serviceWorker.js
    - added components folder
    - modify readme
    - add coverage script to package.json
    - modify eslintrc.js with new rule to check eol max to be 1
    
    [DELIVERS #160617625]

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    mocha: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "import"],
+  rules: {
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "eol-last": 1,
+    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1}]
+  },
+  settings: {
+    react: {
+      version: "latest"
+    }
+  }
+};
