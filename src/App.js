514dbe62b24c686af81782086d78bd1b0602311c
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2018-10-31 07:09:37 +0300

    ch(sass): setup sass pre-processing
    - add node-sass dependency to package.json
    - add sass-loader dependency to package.json
    - add sass/css folder with sample file
    - add sample scss and sass file to style the default div
    - add a classname to div in App.js
    - add assets folder to repo for external dependencies
    - change indentation rule to 2 spaces
    
    [DELIVERS #160617630]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-
+import "./assets/css/App.scss";
 class App extends Component {
-	render() {
-		return (
-			<div><h1>Authors Haven </h1></div>
-		);
-	}
+  render() {
+    return (
+      <div className="app">
+        <h1>Authors Haven </h1>
+        <h2 className='mission'>A Social platform for the creative at heart.</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
